163e7b08a7e8d6522cdc11736f14f4f35f531cc9
Romain Crestey <rcrestey@gmail.com>
2023-09-06 16:30:06 +0200

    suppression correction exo

--- exos/2_browser/2-4_forms/index.js
+++ exos/2_browser/2-4_forms/index.js
@@ -6,31 +6,6 @@ console.log('Exos 3-4: Formulaires');
  *========================================================================**/
 console.log('*** Couleurs ***');
 
-const form = document.getElementById('info');
-
-form.addEventListener('submit', e => {
-  e.preventDefault(); // permet d'éviter de le rechargement de la page
-  const data = [...new FormData(form)]; // lit le contenu du formulaire
-
-  const person = {}; // formattage de la donnée
-  data.forEach(([key, value]) => {
-    person[key] = value;
-  });
-
-  console.log(person);
-  const name = document.createElement('div');
-  const age = document.createElement('div');
-  const article = document.createElement('article');
-  name.textContent = person.name;
-  age.textContent = person.age + ' ans';
-  article.append(name, age);
-  article.style.background = person.color;
-
-  article.addEventListener('click', () => article.remove());
-
-  document.body.append(article);
-});
-
 /**
  * 1) À la soumission du formulaire, construire une carte d'identité affichant:
  * - le nom
